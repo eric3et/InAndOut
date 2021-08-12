3474e058f268dfabe04ab0562811824eb0124ef8
Eric Taft <eric3et@gmail.com>
2021-08-12 13:54:02 -0700

    Add project files.

---
+++ InAndOut/Controllers/AppointmentController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [Route("details/{id?}")]
+        public IActionResult Details(int id)
+        {
+            return Ok($"You have entered id = " + id);
+        }
+
+
+
+
+
+
+    }
+}
---
+++ InAndOut/Controllers/ItemController.cs
@@ -0,0 +1,108 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class ItemController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+
+        public ItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            IEnumerable<Item> objList = _db.Items;
+            return View(objList);
+        }
+
+
+        // GET - Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST - Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Item obj)
+        {
+            _db.Items.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        // GET - Delete
+        public IActionResult Delete(int ?id)
+        {
+            var obj = _db.Items.Find(id);
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // POST - Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int ?id)
+        {
+            var obj = _db.Items.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Items.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        // GET - Update
+        public IActionResult Update(int? id)
+        {
+            var obj = _db.Items.Find(id);
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // POST - Update
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Item obj)
+        {
+            _db.Items.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
