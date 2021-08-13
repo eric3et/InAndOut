01a9d26a7f8105be275a457858d21d2fc4353fb0
Eric Taft <eric3et@gmail.com>
2021-08-12 17:19:04 -0700

    update

--- InAndOut/wwwroot/js/site.js
+++ InAndOut/wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function increment(obj) {
+    var id = obj.id
+
+
+
+    var btn = document.getElementById(id)
+
+
+    var out = parseInt(btn.innerText)
+    var x = Math.random() * 256
+    var y = Math.random() * 256
+    var z = Math.random() * 256
+    btn.style.background = "rgb(" + x + "," + y + "," + z + ")"
+    out++;
+    btn.innerText = out;
+    if ((x + y + z) < ((255 * 3) / 2)) {
+        btn.style.color = "white"
+    }
+    else {
+        btn.style.color = "black"
+    }
+
+
+
+}
